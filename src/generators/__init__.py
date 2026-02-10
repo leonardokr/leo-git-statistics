7f0616e775be96307dc04dcd3b563fcbe197b0a2
Leonardo K. R. <leo@ziondev.us>
2026-02-09 21:26:59 -0300

    feat(generators): export LanguagesPuzzleGenerator

--- src/generators/__init__.py
+++ src/generators/__init__.py
@@ -1,6 +1,7 @@
 from src.generators.base import BaseGenerator
 from src.generators.overview import OverviewGenerator
 from src.generators.languages import LanguagesGenerator
+from src.generators.languages_puzzle import LanguagesPuzzleGenerator
 from src.generators.streak import StreakGenerator
 from src.generators.streak_battery import StreakBatteryGenerator
 
@@ -8,6 +9,7 @@ __all__ = [
     "BaseGenerator",
     "OverviewGenerator",
     "LanguagesGenerator",
+    "LanguagesPuzzleGenerator",
     "StreakGenerator",
     "StreakBatteryGenerator",
 ]
